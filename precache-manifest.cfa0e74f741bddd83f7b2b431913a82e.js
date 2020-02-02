dc248e03b17c7afc03eb79224fd472314192433c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-02 13:22:12 +0000

    Deploy abdurashidovtaha/tariffs to github.com/abdurashidovtaha/tariffs.git:gh-pages

---
+++ precache-manifest.cfa0e74f741bddd83f7b2b431913a82e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a341fa9f7d025db06e14afce9cd99f55",
+    "url": "/tariffs/index.html"
+  },
+  {
+    "revision": "4d6a5671ff5d8e8e4e29",
+    "url": "/tariffs/static/css/main.2c835918.chunk.css"
+  },
+  {
+    "revision": "b5bc3d37c73adb2943c0",
+    "url": "/tariffs/static/js/2.c643a220.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tariffs/static/js/2.c643a220.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d6a5671ff5d8e8e4e29",
+    "url": "/tariffs/static/js/main.9849f5da.chunk.js"
+  },
+  {
+    "revision": "01eaa79565fcc1bd8fd5",
+    "url": "/tariffs/static/js/runtime-main.b6e0952f.js"
+  }
+]);
